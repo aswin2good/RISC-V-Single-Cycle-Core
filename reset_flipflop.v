3f53de3179cb588cb0bf471b3222d610c875b106
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-02 23:38:36 +0530

    Update reset_flipflop.v

--- reset_flipflop.v
+++ reset_flipflop.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/02/2025 11:01:25 PM
-// Design Name: 
-// Module Name: reset_flipflop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module reset_flipflop  (
     input       clk, rst,
