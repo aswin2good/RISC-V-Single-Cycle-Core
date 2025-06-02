e6885fea4966e0280dcd15c373179a780fbdfcd5
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-02 23:38:13 +0530

    Update mux_2to1.v

--- mux_2to1.v
+++ mux_2to1.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/02/2025 11:02:55 PM
-// Design Name: 
-// Module Name: mux_2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module mux_2to1 (
     input       [31:0] d0, d1,
