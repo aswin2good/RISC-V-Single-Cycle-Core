4ce3d08acd744bc0c82b49cb0675f4fcd942697f
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-02 23:37:45 +0530

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/02/2025 11:05:00 PM
-// Design Name: 
-// Module Name: datapath
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module datapath(
     input         clk, reset,                     // Clock and reset for sequential logic
