bad5d2288d55e1e583757d4e6cfa7f2b45ef8930
Achutha Aswin Naick <aswin.gcsj@gmail.com>
2025-06-02 23:37:26 +0530

    Update controller.v

--- controller.v
+++ controller.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/02/2025 10:42:27 PM
-// Design Name: 
-// Module Name: controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module controller (
     input  [6:0] opcode,         // opcode
